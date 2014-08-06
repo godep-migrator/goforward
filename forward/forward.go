403ba5f5b33184d8b306db1dd5d7aaf9ce950d06
Ben Aldrich <vrecan@gmail.com>
2014-08-05 21:19:10 -0600

    add full wiring , fully wired!

--- forward/forward.go
+++ forward/forward.go
@@ -1,12 +1,41 @@
 package forward
 
 import (
-	"github.com/CapillarySoftware/goforward/msgService"
+	"code.google.com/p/go-uuid/uuid"
+	"github.com/CapillarySoftware/goforward/messaging"
 	log "github.com/cihub/seelog"
+	nano "github.com/op/go-nanomsg"
 )
 
-func Run(channel <-chan msgService.ForwardMessage) {
+const ()
+
+func Run(channel <-chan messaging.Food) {
+	socket, err := nano.NewPushSocket()
+	if nil != err {
+		log.Error(err)
+	}
+	defer socket.Close()
+	_, err = socket.Connect("tcp://localhost:2025")
+	if nil != err {
+		log.Error(err)
+		return
+	}
 	for msg := range channel {
+		index := "document"
+		indexType := "all"
+		id := string(uuid.NewRandom())
+		msg.Index = &index
+		msg.IndexType = &indexType
+		msg.Id = &id
 		log.Trace(msg)
+		bytes, err := msg.Marshal()
+		if nil != err {
+			log.Error(err)
+			continue
+		}
+		_, err = socket.Send(bytes, nano.DontWait) //blocking
+		if nil != err {
+			log.Error(err)
+		}
 	}
 }
