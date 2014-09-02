54d1f2c6faca59dfbf39bdd8cf2f873b38f0b5a7
Ben Aldrich <vrecan@gmail.com>
2014-09-02 08:35:24 -0600

    turn registration into go routine

--- goforward.go
+++ goforward.go
@@ -4,9 +4,12 @@ import (
 	"github.com/CapillarySoftware/goforward/start"
 	consul "github.com/armon/consul-api"
 	log "github.com/cihub/seelog"
+	"strconv"
+	"time"
 )
 
 func main() {
+
 	defer log.Flush()
 	logger, err := log.LoggerFromConfigAsFile("seelog.xml")
 
@@ -14,32 +17,52 @@ func main() {
 		log.Warn("Failed to load config", err)
 	}
 
+	log.ReplaceLogger(logger)
+	go RegisterService("goforward", 2025, 15)
+	start.Run()
+}
+
+func RegisterService(name string, port int, ttl int) {
+
+	reportInterval := make(chan bool, 1)
+	go func() {
+		for {
+			time.Sleep(time.Duration(ttl) / 2 * time.Second)
+			reportInterval <- true
+		}
+	}()
+
 	client, err := consul.NewClient(consul.DefaultConfig())
 	if nil != err {
 		log.Error("Failed to get consul client")
-	} else {
-
-		agent := client.Agent()
-
-		reg := &consul.AgentServiceRegistration{
-			Name: "goforward",
-			Port: 2025,
-			Check: &consul.AgentServiceCheck{
-				TTL: "10s",
-			},
-		}
-		if err := agent.ServiceRegister(reg); err != nil {
-			log.Error("err: ", err)
-		}
-		checks, err := agent.Checks()
-		if err != nil {
-			log.Error("err: ", err)
-		}
-		if _, ok := checks["goforward"]; !ok {
-			log.Error("Checks failed:, ", checks)
-		}
-
 	}
-	log.ReplaceLogger(logger)
-	start.Run()
+
+	for {
+		select {
+		case <-reportInterval: //report registration
+			{
+
+				agent := client.Agent()
+
+				reg := &consul.AgentServiceRegistration{
+					Name: "goforward",
+					Port: port,
+					Check: &consul.AgentServiceCheck{
+						TTL: strconv.Itoa(ttl) + "s",
+					},
+				}
+				if err := agent.ServiceRegister(reg); err != nil {
+					log.Error("err: ", err)
+				}
+				checks, err := agent.Checks()
+				if err != nil {
+					log.Error("err: ", err)
+				}
+				if _, ok := checks["goforward"]; !ok {
+					log.Error("Checks failed:, ", checks)
+				}
+			}
+		}
+	}
+
 }
