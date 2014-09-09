9386f6814cb1fcc523ef00092b446f9dadbe4024
Ben Aldrich <vrecan@gmail.com>
2014-09-09 15:05:09 -0600

    update to just build and revert other changes

--- ansible_install.sh
+++ ansible_install.sh
@@ -4,14 +4,7 @@ export GOPATH=/home/capillaryDeploy/go
 export GOROOT=/usr/local/go
 export GOBIN=/home/capillaryDeploy/go/bin
 export PATH=$PATH:$GOBIN:$GOROOT/bin
-dir=$GOPATH/src/github.com/CapillarySoftware/goforward
-install=/usr/local/perceptor/goforward
-mkdir -p $install
-mkdir -p $dir
-cd $dir
 go get github.com/tools/godep
 go install github.com/tools/godep
 godep restore
-godep go build
-cp goforward  $install/
-cp seelog.xml $install/
+godep go build github.com/CapillarySoftware/goforward
