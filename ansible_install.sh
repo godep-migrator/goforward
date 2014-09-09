2d5a7f95909f6cd8f4955a10d2b14ea645c0b3ab
Ben Aldrich <vrecan@gmail.com>
2014-09-09 14:59:07 -0600

    don't remove everything yet

--- ansible_install.sh
+++ ansible_install.sh
@@ -13,8 +13,7 @@ go get github.com/tools/godep
 go install github.com/tools/godep
 godep restore
 godep go build
-mkdir -p $install
 cp goforward  $install/
 cp seelog.xml $install/
 //remove all source code after install
-rm -rf $GOPATH
+#rm -rf $GOPATH
